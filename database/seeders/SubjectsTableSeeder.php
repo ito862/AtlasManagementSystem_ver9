a9d06de2f3328a32817535fcc138b8398d482382
伊藤　光祐 <itokosuke.jbm@gmail.com>
2025-03-09 20:58:21 +0900

    シードに初期データ追加、SubjectsTableSeederに「国語」、「数学」、「英語」のレコードを作成

--- database/seeders/SubjectsTableSeeder.php
+++ database/seeders/SubjectsTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class SubjectsTableSeeder extends Seeder
 {
@@ -15,5 +16,10 @@ class SubjectsTableSeeder extends Seeder
     public function run()
     {
         // 国語、数学、英語を追加
+        DB::table('subjects')->insert([
+            ['id' => 1, 'subject' => '国語'],
+            ['id' => 2, 'subject' => '数学'],
+            ['id' => 3, 'subject' => '英語']
+        ]);
     }
 }
--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -1,8 +1,11 @@
 <?php
+
 namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UsersTableSeeder extends Seeder
 {
@@ -13,6 +16,23 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-
+        DB::table('users')->insert(
+            [
+                'id' => 1,
+                'over_name' => 'コンパス',
+                'under_name' => '太郎',
+                'over_name_kana' => 'コンパス',
+                'under_name_kana' => 'タロウ',
+                'mail_address' => 'kaishukadai@email.com',
+                'sex' => '1',
+                'birth_day' => '2000-01-01',
+                'role' => '3',
+                'password' => Hash::make('test8833'),
+                'remember_token' => null,
+                'created_at' => now(),
+                'updated_at' => now(),
+                'deleted_at' => null,
+            ]
+        );
     }
 }
