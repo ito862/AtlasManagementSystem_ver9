4425461ad49a9d8a7d734bee5e5c37fecedf97d8
伊藤　光祐 <itokosuke.jbm@gmail.com>
2025-03-22 18:11:09 +0900

    ログインタイムアウトでloginへ遷移するように変更

--- app/Http/Controllers/Authenticated/Users/UsersController.php
+++ app/Http/Controllers/Authenticated/Users/UsersController.php
@@ -10,32 +10,77 @@ use App\Models\Users\User;
 use App\Models\Users\Subjects;
 use App\Searchs\DisplayUsers;
 use App\Searchs\SearchResultFactories;
+use Illuminate\Support\Facades\Auth;
 
 class UsersController extends Controller
 {
 
-    public function showUsers(Request $request){
+    public function showUsers(Request $request)
+    {
         $keyword = $request->keyword;
         $category = $request->category;
         $updown = $request->updown;
         $gender = $request->sex;
         $role = $request->role;
-        $subjects = null;// ここで検索時の科目を受け取る
+        $subjects = null; // ここで検索時の科目を受け取る
         $userFactory = new SearchResultFactories();
         $users = $userFactory->initializeUsers($keyword, $category, $updown, $gender, $role, $subjects);
         $subjects = Subjects::all();
         return view('authenticated.users.search', compact('users', 'subjects'));
     }
 
-    public function userProfile($id){
+    public function userProfile($id)
+    {
         $user = User::with('subjects')->findOrFail($id);
         $subject_lists = Subjects::all();
         return view('authenticated.users.profile', compact('user', 'subject_lists'));
     }
 
-    public function userEdit(Request $request){
+    public function userEdit(Request $request)
+    {
         $user = User::findOrFail($request->user_id);
         $user->subjects()->sync($request->subjects);
         return redirect()->route('user.profile', ['id' => $request->user_id]);
+    }
+    // ユーザー検索
+    // 検索条件
+    // 名前、性別、役職、選択科目
+    // 並び替え条件：IDの昇降順、名前昇降順
+    public function userSearch(Request $request)
+    {
+        // 検索フォームに入力された値を取得
+        $keyword = $request->input('keyword');
+        $sex = $request->input('sex');
+        $role = $request->input('role');
+        $subject = $request->input('subject');
+
+        // テーブル結合してデータを取得
+        $query = User::select('users.*')
+            ->join('subject_user', 'users.id', '=', 'subject_user.user_id')
+            ->join('subjects', 'subject_user.subject_id', '=', 'subjects.id');
+
+
+        // 仮組名前は４種類あるので模索する
+        if (!empty($keyword)) {
+            $query->where('over_name', 'like', '%' . $keyword . '%')->get();
+        }
+
+        if (!empty($sex)) {
+            $query->where('sex', 'like', $sex);
+        }
+
+        if (!empty($role)) {
+            $query->where('role', 'like', $role);
+        }
+        // subjectはID取得？
+        // 複数の科目で検索した場合、あてはまる科目を一つでも選択しているユーザーはすべて表示させる
+        if ($request->filled('subject')) {
+            $query->where('subjects.subject', 'like', '%' . $request->subject . '%');
+        }
+
+        // 結果を取得
+        $users = $query->get();
+
+        return view('/users/search', ['users' => $users]);
     }
 }
--- resources/views/authenticated/users/search.blade.php
+++ resources/views/authenticated/users/search.blade.php
@@ -1,94 +1,103 @@
 <x-sidebar>
-<p>ユーザー検索</p>
-<div class="search_content w-100 border d-flex">
-  <div class="reserve_users_area">
-    @foreach($users as $user)
-    <div class="border one_person">
-      <div>
-        <span>ID : </span><span>{{ $user->id }}</span>
+  <!-- ユーザー表示 -->
+  <p>ユーザー検索</p>
+  <div class="search_content w-100 border d-flex">
+    <div class="reserve_users_area">
+      @foreach($users as $user)
+      <div class="border one_person">
+        <div>
+          <span>ID : </span><span>{{ $user->id }}</span>
+        </div>
+        <div><span>名前 : </span>
+          <a href="{{ route('user.profile', ['id' => $user->id]) }}">
+            <span>{{ $user->over_name }}</span>
+            <span>{{ $user->under_name }}</span>
+          </a>
+        </div>
+        <div>
+          <span>カナ : </span>
+          <span>({{ $user->over_name_kana }}</span>
+          <span>{{ $user->under_name_kana }})</span>
+        </div>
+        <div>
+          @if($user->sex == 1)
+          <span>性別 : </span><span>男</span>
+          @elseif($user->sex == 2)
+          <span>性別 : </span><span>女</span>
+          @else
+          <span>性別 : </span><span>その他</span>
+          @endif
+        </div>
+        <div>
+          <span>生年月日 : </span><span>{{ $user->birth_day }}</span>
+        </div>
+        <div>
+          @if($user->role == 1)
+          <span>権限 : </span><span>教師(国語)</span>
+          @elseif($user->role == 2)
+          <span>権限 : </span><span>教師(数学)</span>
+          @elseif($user->role == 3)
+          <span>権限 : </span><span>講師(英語)</span>
+          @else
+          <span>権限 : </span><span>生徒</span>
+          @endif
+        </div>
+        <!-- ここの部分要確認中間テーブルの情報取得 -->
+        <div>
+          @if($user->role == 4)
+          <span>選択科目 : </span><span>国語</span>
+          @endif
+        </div>
       </div>
-      <div><span>名前 : </span>
-        <a href="{{ route('user.profile', ['id' => $user->id]) }}">
-          <span>{{ $user->over_name }}</span>
-          <span>{{ $user->under_name }}</span>
-        </a>
-      </div>
-      <div>
-        <span>カナ : </span>
-        <span>({{ $user->over_name_kana }}</span>
-        <span>{{ $user->under_name_kana }})</span>
-      </div>
-      <div>
-        @if($user->sex == 1)
-        <span>性別 : </span><span>男</span>
-        @elseif($user->sex == 2)
-        <span>性別 : </span><span>女</span>
-        @else
-        <span>性別 : </span><span>その他</span>
-        @endif
-      </div>
-      <div>
-        <span>生年月日 : </span><span>{{ $user->birth_day }}</span>
-      </div>
-      <div>
-        @if($user->role == 1)
-        <span>権限 : </span><span>教師(国語)</span>
-        @elseif($user->role == 2)
-        <span>権限 : </span><span>教師(数学)</span>
-        @elseif($user->role == 3)
-        <span>権限 : </span><span>講師(英語)</span>
-        @else
-        <span>権限 : </span><span>生徒</span>
-        @endif
-      </div>
-      <div>
-        @if($user->role == 4)
-        <span>選択科目 :</span>
-        @endif
-      </div>
+      @endforeach
     </div>
-    @endforeach
-  </div>
-  <div class="search_area w-25 border">
-    <div class="">
-      <div>
-        <input type="text" class="free_word" name="keyword" placeholder="キーワードを検索" form="userSearchRequest">
-      </div>
-      <div>
-        <lavel>カテゴリ</lavel>
-        <select form="userSearchRequest" name="category">
-          <option value="name">名前</option>
-          <option value="id">社員ID</option>
-        </select>
-      </div>
-      <div>
-        <label>並び替え</label>
-        <select name="updown" form="userSearchRequest">
-          <option value="ASC">昇順</option>
-          <option value="DESC">降順</option>
-        </select>
-      </div>
+    <!-- 検索機能 -->
+    <div class="search_area w-25 border">
       <div class="">
-        <p class="m-0 search_conditions"><span>検索条件の追加</span></p>
-        <div class="search_conditions_inner">
-          <div>
-            <label>性別</label>
-            <span>男</span><input type="radio" name="sex" value="1" form="userSearchRequest">
-            <span>女</span><input type="radio" name="sex" value="2" form="userSearchRequest">
-            <span>その他</span><input type="radio" name="sex" value="3" form="userSearchRequest">
-          </div>
-          <div>
-            <label>権限</label>
-            <select name="role" form="userSearchRequest" class="engineer">
-              <option selected disabled>----</option>
-              <option value="1">教師(国語)</option>
-              <option value="2">教師(数学)</option>
-              <option value="3">教師(英語)</option>
-              <option value="4" class="">生徒</option>
-            </select>
-          </div>
-          <div class="selected_engineer">
-            <label>選択科目</label>
+        <div>
+          <input type="text" class="free_word" name="keyword" placeholder="キーワードを検索" form="userSearchRequest">
+        </div>
+        <div>
+          <label>カテゴリ</label>
+          <select form="userSearchRequest" name="category">
+            <option value="name">名前</option>
+            <option value="id">社員ID</option>
+          </select>
+        </div>
+        <div>
+          <label>並び替え</label>
+          <select name="updown" form="userSearchRequest">
+            <option value="ASC">昇順</option>
+            <option value="DESC">降順</option>
+          </select>
+        </div>
+
+        <!-- 検索条件の追加 -->
+        <div class="">
+          <p class="m-0 search_conditions"><span>検索条件の追加</span></p>
+          <div class="search_conditions_inner">
+            <div>
+              <label>性別</label>
+              <span>男</span><input type="radio" name="sex" value="1" form="userSearchRequest">
+              <span>女</span><input type="radio" name="sex" value="2" form="userSearchRequest">
+              <span>その他</span><input type="radio" name="sex" value="3" form="userSearchRequest">
+            </div>
+            <div>
+              <label>権限</label>
+              <select name="role" form="userSearchRequest" class="engineer">
+                <option selected disabled>----</option>
+                <option value="1">教師(国語)</option>
+                <option value="2">教師(数学)</option>
+                <option value="3">教師(英語)</option>
+                <option value="4" class="">生徒</option>
+              </select>
+            </div>
+            <div class="selected_engineer">
+              <label>選択科目</label>
+              <span>国語</span><input type="checkbox" name="subject" value="1" form="userSearchRequest">
+              <span>数学</span><input type="checkbox" name="subject" value="2" form="userSearchRequest">
+              <span>英語</span><input type="checkbox" name="subject" value="3" form="userSearchRequest">
+            </div>
           </div>
         </div>
       </div>
@@ -101,5 +110,5 @@
     </div>
     <form action="{{ route('user.show') }}" method="get" id="userSearchRequest"></form>
   </div>
-</div>
+  </div>
 </x-sidebar>
